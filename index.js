a3510b09d23a3ca3457c1389d64d8f6b867ee878
Maingi <78572304+MaingiMuema@users.noreply.github.com>
2022-05-25 19:03:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+//get the form by its id
+const form = document.getElementById("contact"); 
+
+const formEvent = form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let mail = new FormData(form);
+
+  sendMail(mail);
+})
+
+const sendMail = (mail) => {
+    
+    fetch("/send", {
+      method: "post", 
+      body: mail, 
+  
+    }).then((response) => {
+      return response.json();
+    });
+  };
